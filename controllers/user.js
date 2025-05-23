cdff73ef0e4353964da50460957b496d8c18de61
Yash Atkari <yashatkari7@gmail.com>
2025-05-23 10:56:20 +0530

    Fix redirect issue

--- controllers/user.js
+++ controllers/user.js
@@ -1,43 +1,49 @@
 const User = require("../models/user");
 
+// Show signup form
 module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
 
-module.exports.signup = async (req, res) => {
+// Handle signup logic
+module.exports.signup = async (req, res, next) => {
     try {
-        let { username, email, password } = req.body;
-        const newUser = new User({email, username});
+        const { username, email, password } = req.body;
+        const newUser = new User({ email, username });
         const registeredUser = await User.register(newUser, password);
+
         req.login(registeredUser, (err) => {
             if (err) {
-              return next(err);
+                return next(err); // Use next to pass to error handler
             }
             req.flash("success", "Welcome to Wanderlust!");
-            res.redirect("/listings");
+            return res.redirect("/listings"); // Add return to prevent header issues
         });
-    } catch(err) {
+    } catch (err) {
         req.flash("error", err.message);
-        res.redirect("/signup");
+        return res.redirect("/signup"); // Add return to ensure no further execution
     }
 };
 
+// Show login form
 module.exports.renderLoginForm = (req, res) => {
     res.render("users/login.ejs");
 };
 
-module.exports.login = async (req, res) => {
+// Handle login logic (relies on Passport.js middleware)
+module.exports.login = (req, res) => {
     req.flash("success", "Welcome back to Wanderlust!");
-    let redirectUrl = res.locals.redirectUrl || "/listings";
-    res.redirect(redirectUrl);
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    return res.redirect(redirectUrl); // Add return
 };
 
+// Handle logout
 module.exports.logout = (req, res, next) => {
     req.logout((err) => {
-      if (err) {
-        return next(err);
-      }
-      req.flash("success", "You are logged out!");
-      res.redirect("/listings");
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "You are logged out!");
+        return res.redirect("/listings"); // Add return
     });
 };
